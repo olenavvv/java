6d923d2f88b6172d59866bf92e8ef67bc115087c
cstuser <cstuser>
2017-02-13 11:49:42 -0500

    Addede .gitignore and moved hello word in a mothod

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,10 +4,13 @@ package javaapplication1;
 public class JavaApplication1 
 {
 
-    
+    public static void hello()
+    {
+        System.out.println("Hello word!");
+    }
     public static void main(String[] args) 
     {
-        System.out.println("Hello word");
+        hello();
     }
     
 }
