45df6429613ff1a3a64cc7792a766aa33d59c604
cstuser <cstuser>
2017-02-13 11:28:17 -0500

    InitialProject

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,13 @@
+
+package javaapplication1;
+
+public class JavaApplication1 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello word");
+    }
+    
+}
